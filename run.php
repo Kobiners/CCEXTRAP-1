8a74baee9f5971bea20458bf6bb73fe7bcc7ec16
Kobiners <cuhmbrres@gmail.com>
2024-12-04 22:23:21 -0600

    Update run.php

--- run.php
+++ run.php
@@ -1,14 +1,13 @@
-<?php
-require "./cc.class.php";
-echo("###############################################\n");
-echo("#     Auto Generate + Checker CC Extrap       #\n");
-echo("###############################################\n");
-echo("# Create By : Kyuoko                          #\n");
-echo("# Code : PHP                                  #\n");
-echo("# Thx To UKL-TEAM, GFS-TEAM, AND YOU          #\n");
-echo("###############################################\n");
-echo "[+] BIN : "; $bin = trim(fgets(STDIN));
-echo "[+] Check Status Valid ? 1/0 : "; $check = trim(fgets(STDIN));
-echo "[+] Amount/Jumlah : "; $amount = trim(fgets(STDIN));
-$new = new CC($bin, $check, $amount);
-echo $new->Execute();
+private function color($text, $color = "default")
+{
+    $arrayColor = array(
+        'grey'         => '1;30',
+        'red'         => '1;31',
+        'green'     => '1;32',
+        'yellow'     => '1;33',
+        'blue'         => '1;34',
+        'purple'     => '1;35',
+        'nevy'         => '1;36',
+        'white'     => '1;0',
+    );
+    return "\033[" . $arrayColor[$color] . "m" . $text . "\033[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/thaotrong/webscan/tree/2803ba5debdaae1f22af3da54e2c3acbcc902822/Modules%2Fwploit-modules.php?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/gasbarengsidoarjo/food169/tree/1bc2375fb4e6a2a0d35d0d7b8f33d50ed8f80af8/function.php?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")
